be3c48337b29f726921118ddd8f8ba9c922d5e8d
cord70 <emd.kornienko@gmail.com>
2025-05-23 20:50:40 +0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,21 @@
+var menu='\
+<h4> сайт cord70.github.io/cyber</h4>\
+  <p><a href="mind/consciousness.html"> Механизмы сознания</a></p>\
+  <p><a href="mind/articles.html"> Статьи о природе сознания</a></p>\
+  <p><a href="ai/ai.html"> О конструкции ощущающей машины</a></p>\
+  <p><a href="world/space.html"> Происхождение пространства</a></p>\
+  <p><a href="science/index.html"> Сложные задачи</a></p>\
+  <p><a href="science/FTL-causality.html"> Пример нарушения причинности</a></p>\
+  <p> &nbsp;</p>\
+  <p><a href="search.html"> Поиск по сайту</a></p>\
+  <p><a href="en.html"> English page</a></p>';
+
+var menuen='\
+<h4> the site cord70.github.io/cyber</h4>\
+  <p><a href="mind/danger-en.html"> Dangerously intelligent</a></p>\
+  <p><a href="mind/prove-en.html"> Consciousness and Logic</a></p>\
+  <p> &nbsp;</p>\
+  <p><a href="world/existence-en.html"> Why is existence possible</a></p>\
+  <p> &nbsp;</p>\
+  <p><a href="search-en.html"> Search on site</a></p>\
+  <p><a href="index.html"> Русская страница</a></p>';
---
+++ sw.js
@@ -0,0 +1,66 @@
+// cyber
+
+var cacheName = 'pwa_v1'; // нужно менять имя при смене контента сайта
+
+var filesToCache = [
+// home page
+    '/index.html',
+
+// home page head
+    '/images/ansimeta.js',
+    '/nav.js',
+    '/images/main.css',
+    '/images/photohome.gif',
+    '/images/sky.jpg',
+    '/images/home.gif',
+    '/manifest.json',
+    '/sw.js',
+    '/favicon.ico',
+    '/favicon.svg',
+    '/apple-touch-icon.png',
+    '/favicon512.png',
+
+// home page links
+    '/mind/index.html',
+    '/ai/ai.html',
+    '/ai/sense.html',
+    '/mind/emergent.html',
+    '/reading/seeing.html',
+    '/mind/how-we-see.html',
+    '/ai-post/future.html',
+    '/ai-post/brain.html',
+    '/ai-post/index.html',
+    '/mind/consciousness.html',
+    '/mind/danger.html',
+    '/mind/articles.html',
+    '/world/index.html',
+    '/world/space.html',
+    '/world/existence.html',
+    '/science/index.html',
+    '/science/hard-problems.html',
+    '/science/FTL-causality.html',
+    '/science/ftl.html',
+    '/science/constants-change.html',
+    '/science/photon_mass.html',
+    '/science/crackpot.html',
+    '/science/measure_c.html',
+    '/pagelist.html',
+];
+
+// Start the service worker and cache all of the app's content 
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// Serve cached content when offline 
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
