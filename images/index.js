34c1b282bad87106329d6f086d073e8c64692c52
cord70 <emd.kornienko@gmail.com>
2025-05-31 23:57:25 +0300

    added index.js

---
+++ images/index.js
@@ -0,0 +1,15 @@
+
+// переход на ../index.html
+  
+const Link = document.querySelector('link[rel="canonical"]');
+const home= "/index.html";
+var url = Link ? Link.href : null;
+  
+if(url!=null && url.endsWith(home))
+  {
+    var dir= url.slice(0, -home.length);
+    dir= dir.slice(Math.max(dir.lastIndexOf('.'),dir.lastIndexOf('/'))+1);
+    url= window.location.href;     
+    if(url.endsWith(dir+"/"))	url= url.slice(0,-1);
+    if(url.endsWith(dir))	window.location.href= url+home; 
+  }
